3ae1ac6afa088a321c91d74e6bca202fd3743ef3
dasooni <dr.amit@live.se>
2023-01-30 23:32:03 +0100

    klar, typ, aa? visst kanske. nej

--- A2.py
+++ A2.py
@@ -7,20 +7,11 @@ import numpy as np
 column = ["x", "y", "letter"]
 data1 = pd.read_csv("data1.csv", header=None, names=column)
 data2 = pd.read_csv("data2.csv", header=None, names=column)
-
-data1_x = data1['x']
-data1_y = data1['y']
-data1_l = data1['letter']
-
-data2_x = data2['x']
-data2_y = data2['y']
-data2_l = data2['letter']
+data_x = data1['x']
+data_y = data1['y']
+data_l = data1['letter'] 
 
 
-data = data1
-data_x = data1_x
-data_y = data1_y
-data_l = data1_l
 
 width = 1000
 height = 800
@@ -32,23 +23,21 @@ y_max = max(data_y)
 
 default_origin = (width / 2, height / 2)
 
+#Abs range scaling, special case for quadrants.
 x_range = max(abs(x_min), abs(x_max)) * 2 + 50
 y_range = max(abs(y_min), abs(y_max)) * 2 + 50
+
 kx = (x_max - x_min) / x_range
 ky = (y_max - y_min) / y_range
+#Max min scaling
+x_scale = (width-80) / (x_max - x_min)
+y_scale = (height- 80) / (y_max - y_min)
 
-
-x_scale = (width-100) / (x_max - x_min)
-y_scale = (height-100) / (y_max - y_min)
-
-# 
 def determine_quadrants(x0, y0, x, y):
     #x0, y0 relative to the screen
     #x, y relative to the data
-
     x = x * kx + x0
     y = y * ky + y0
-
 
     if x > x0 and y < y0:
         return 1
@@ -60,8 +49,6 @@ def determine_quadrants(x0, y0, x, y):
         return 4
     else:
         return 0
-
-
 
 def determine_quadrant_color(quadrant):
     if quadrant == 1:
@@ -81,13 +68,13 @@ def euclidean_distance(x0, y0, x, y):
 
     return np.sqrt((x0-x)**2 + (y0-y)**2)
 
-
 colors = []
 colors2letters = {}
 def determine_color(data_l, letter):
-    colors = ['red', 'blue', 'green', 'yellow', 'white', 'black', 'cyan']
+    colors = ['blue', 'red', 'yellow', 'green', 'white', 'black', 'cyan']
     colors2letters = {letter: colors[i % len(colors)]
                       for i, letter in enumerate(data_l.unique())}
+
     return colors2letters[letter]
 
 def unique_letters(data_l):
@@ -98,17 +85,21 @@ def draw_legend(canvas, data_l):
     legend = tk.Canvas(canvas, width=100, height=100,
                        borderwidth=2, relief="groove")
     legend.place(x=20, y=30)
-    # add a legend
+
     legend_x = 15
     legend_y = 15
 
     # draw the legend
-    for letter in set(data_l.unique()):
+    for letter in unique_letters(data_l):
+        legend.create_oval(legend_x, legend_y, legend_x +
+                                20, legend_y + 20, fill=determine_color(data_l, letter))
         legend.create_rectangle(legend_x, legend_y, legend_x +
                                 20, legend_y + 20, fill=determine_color(data_l, letter))
+        legend.create_polygon(legend_x, legend_y, legend_x +
+                                20, legend_y + 20, fill=determine_color(data_l, letter))                       
+
         legend.create_text(legend_x + 25, legend_y + 10, text=letter)
         legend_y += 30
-
 
 def draw_tickers(canvas, x_scale, y_scale, xmin, xmax, ymin, ymax):
     for i in range(int(xmin), int(xmax), 10):
@@ -142,19 +133,16 @@ def draw_tickers(canvas, x_scale, y_scale, xmin, xmax, ymin, ymax):
         canvas.create_text(
             width / 2 + 20, height / 2 + i * y_scale, text=str(-i), anchor=tk.E)
 
-xAxis = []
-yAxis = []
+
 def draw_axes(canvas, x0, y0):
     # draw the axes
     canvas.create_line(0, y0, width, y0)
     canvas.create_line(x0, 0, x0, height)
 
-
-
 px = 5
+points_id = []
 def draw_data(canvas, data_x, data_y, data_l, x_scale, y_scale, x0, y0):
-    global points_id 
-    points_id = []
+    tags = ["clickable", "circle", "square", "triangle", "oval"]
     # draw the data
     for i in range(len(data_x)):
         x = data_x[i]
@@ -165,22 +153,23 @@ def draw_data(canvas, data_x, data_y, data_l, x_scale, y_scale, x0, y0):
         #Different shapes for each unique letter
         if letter == data_l.unique()[0]:
             points_id.append(canvas.create_oval(x0 + x * x_scale - px, y0- y * y_scale - px,
-                               x0 + x * x_scale + px, y0- y * y_scale + px, fill=color, tag="clickable"))
+                               x0 + x * x_scale + px, y0- y * y_scale + px, fill=color, tag=tags))
             
         elif letter == data_l.unique()[1]:
             points_id.append(canvas.create_rectangle(x0 + x * x_scale - px, y0 - y * y_scale - px,
-                                    x0 + x * x_scale + px, y0 - y * y_scale + px, fill=color, tag="clickable"))
+                                    x0 + x * x_scale + px, y0 - y * y_scale + px, fill=color, tag=tags))
         elif letter == data_l.unique()[2]:
             points_id.append(canvas.create_polygon(x0 + x * x_scale - px, y0 - y * y_scale - px,
                                   x0 + x * x_scale + px, y0- y * y_scale - px,
-                                  x0 + x * x_scale, y0 - y * y_scale + px, fill=color, tag="clickable"))
+                                  x0 + x * x_scale, y0 - y * y_scale + px, fill=color, tag=tags))
         else: 
             points_id.append(canvas.create_oval(x0 + x * x_scale - px, y0- y * y_scale - px,
-                               x0 + x * x_scale + px, y0- y * y_scale + px, fill=color, tag="clickable"))
-        
+                               x0 + x * x_scale + px*1.1, y0- y * y_scale + px*1.1, fill=color, tag=tags))
         canvas.create_text(x0 + x * x_scale, y0 - y * y_scale + 15, text=letter, font = "Times 10")
     
+    
     print("ids of the points: ", points_id)
+
 # create the window
 window = tk.Tk()
 window.title("A2")
@@ -196,39 +185,42 @@ def draw_on_start():
     draw_legend(canvas, data_l)
     draw_data(canvas, data_x, data_y, data_l, x_scale, y_scale, 
     default_origin[0], default_origin[1])
+    
 
 def change_data():
-
     #TODO: Implement read_csv(csv), min_max_scale(min, max, data)
     #Scaling does not work properly...
+    #Clear the canvas
+    canvas.delete("all")
+    #hardcoding YES SIR
     data2 = pd.read_csv("data2.csv", header=None, names=column)
-    data_x, data_y, data_l = data2.iloc[:, 0], data2.iloc[:, 1], data2.iloc[:, 2]
+    data_x = data2['x']
+    data_y = data2['y']
+    data_l = data2['letter'] 
+
     x_min = data_x.min()
     x_max = data_x.max()
     y_min = data_y.min()
     y_max = data_y.max()
-    x_scale = (width - 100) / (x_max - x_min)
-    y_scale = (height - 100) / (y_max - y_min)
-    
 
-    #Clear the canvas
-    canvas.delete("all")
+    x_scale = (width/2.1) / (x_max - x_min)
+    y_scale = (height/4) / (y_max - y_min)
 
     # Redraw x- and y-axis
-    canvas.create_line(0, default_origin[1], width - 50, default_origin[1])
-    canvas.create_line(default_origin[0], 0, default_origin[0], height - 50)
-
+    draw_axes(canvas, default_origin[0], default_origin[1])
     # Redraw tickers
     draw_tickers(canvas, x_scale, y_scale, x_min, x_max, y_min, y_max)
-
     # Redraw legend
     draw_legend(canvas, data_l)
 
     # Redraw data + add origo point
+    draw_data(canvas, data_x, data_y, data_l, x_scale, y_scale, 
+    default_origin[0], default_origin[1])
+
     canvas.create_rectangle(default_origin[0] - 10, default_origin[1] - 10,
         default_origin[0] + 10, default_origin[1] + 10, fill='red', tag="origo")
-    draw_data(canvas, data_x, data_y, data_l, x_scale, y_scale, default_origin[0], default_origin[1])
 
+    window.update()
     
 def on_left_click(event):
     #New origin is the point where the user clicked
@@ -242,23 +234,20 @@ def on_left_click(event):
     #Clear the canvas
     canvas.delete("all")
 
-    canvas.create_oval(x0 - 8, y0 - 8, x0 + 8, y0 + 8, fill='magenta', outline='black')
+    canvas.create_oval(x0 - 12, y0 - 12, x0 + 12, y0 + 12,outline='red', width=5, tag="origo")
     # Redraw x- and y-axis
     canvas.create_line(0, y0, width - 50, y0)
     canvas.create_line(x0, 0, x0, height - 50)
-
     # Redraw tickers
     draw_tickers(canvas, x_scale, y_scale, x_min, x_max, y_min, y_max)
-
     # Redraw legend
     draw_legend(canvas, data_l)
 
-    # Redraw data + add origo point
+
+    # Redraw data + add origo point to return to default state (data1)
     canvas.create_rectangle(default_origin[0] - 10, default_origin[1] - 10, 
         default_origin[0] + 10, default_origin[1] + 10, fill='red', tag="origo")
     draw_data(canvas, data_x, data_y, data_l, x_scale, y_scale, x0, y0)
-
-
 
 
 def on_right_click(event):
@@ -272,15 +261,17 @@ def on_right_click(event):
         distance = euclidean_distance(event.x, event.y, data_x[i], data_y[i]) #euclidean distance between the point and the data point
         closest_points.append(distance)
 
-    closest_points = np.argsort(closest_points)[:5] # INDEX!
-    print("closest points sorted: ", closest_points)
-    print("x :", data_x[closest_points])
-
+    closest_points = np.argsort(closest_points)[:6] # INDEX!
     # Change the color of the five closest points to green
     for i in closest_points:
-        canvas.itemconfig(points_id[i], fill='green')
-        #canvas.create_line(event.x, event.y,
-                                    #event.x + data_x[i] * x_scale + 5, event.y - data_y[i] * y_scale + 5, fill='red')
+        canvas.itemconfig(points_id[i], outline = 'red', width = 2,fill='green', tag ='origo')
+
+    # Draw a line between the clicked point and the five closest points
+    #for i in closest_points:
+     #   canvas.create_line(event.x, event.y, data_x[i] * kx + event.x, event.y + data_y[i] * ky, fill='red')
+
+    canvas.create_rectangle(default_origin[0] - 10, default_origin[1] - 10, 
+    default_origin[0] + 10, default_origin[1] + 10, fill='red', tag="origo")
 
 # Return to the default view
 def reset(event):
---
+++ readme.md
@@ -0,0 +1,5 @@
+Left & right mouse click are bound to the first and the second feature, respectively. 
+
+Left double click on the selected feature deactives the selected feature.
+
+The red square in the middle resets the view to the initial state.
